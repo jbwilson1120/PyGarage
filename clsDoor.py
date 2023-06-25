3e5e146d6880ba533a72764b493619d00a23c28c
jbwilson1120@gmail.com <jbwilson1120@gmail.com>
2023-06-24 23:05:08 -0400

    test

--- clsDoor.py
+++ clsDoor.py
@@ -82,9 +82,9 @@ class clsDoor(object):
 
         print(self.name + " PinSensorOpen = " + str(GPIO.input(self.PinSensorOpen)))
         print(self.name + " PinSensorClosed = " + str(GPIO.input(self.PinSensorClosed)))
-        if GPIO.input(self.PinSensorOpen) == GPIO.LOW:
+        if GPIO.input(self.PinSensorOpen) == 0:
             return "open"
-        elif GPIO.input(self.PinSensorClosed) == GPIO.LOW:
+        elif GPIO.input(self.PinSensorClosed) == 0:
             return "closed"
         else:
             return "unknown"
