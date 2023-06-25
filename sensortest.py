b3033aac33849d987f209de87f64e419c3507bc0
jbwilson1120@gmail.com <jbwilson1120@gmail.com>
2023-06-24 23:24:06 -0400

    test

--- sensortest.py
+++ sensortest.py
@@ -6,12 +6,12 @@ import pinconfig
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)    # the pin numbers refer to the board connector not the chip
 GPIO.setwarnings(False)
-GPIO.setup(pinconfig.DOOR1_CLOSED_SENSOR, GPIO.IN, GPIO.PUD_UP)  # Door is Closed sensor
-GPIO.setup(pinconfig.DOOR1_OPEN_SENSOR, GPIO.IN, GPIO.PUD_UP)    # Door is Open sensor
-GPIO.setup(pinconfig.DOOR2_CLOSED_SENSOR, GPIO.IN, GPIO.PUD_UP)  # Door is Closed sensor
-GPIO.setup(pinconfig.DOOR2_OPEN_SENSOR, GPIO.IN, GPIO.PUD_UP)    # Door is Open sensor
-GPIO.setup(pinconfig.DOOR3_CLOSED_SENSOR, GPIO.IN, GPIO.PUD_UP)  # Door is Closed sensor
-GPIO.setup(pinconfig.DOOR3_OPEN_SENSOR, GPIO.IN, GPIO.PUD_UP)    # Door is Open sensor
+GPIO.setup(pinconfig.DOOR1_CLOSED_SENSOR, GPIO.IN, GPIO.PUD_DOWN)  # Door is Closed sensor
+GPIO.setup(pinconfig.DOOR1_OPEN_SENSOR, GPIO.IN, GPIO.PUD_DOWN)    # Door is Open sensor
+GPIO.setup(pinconfig.DOOR2_CLOSED_SENSOR, GPIO.IN, GPIO.PUD_DOWN)  # Door is Closed sensor
+GPIO.setup(pinconfig.DOOR2_OPEN_SENSOR, GPIO.IN, GPIO.PUD_DOWN)    # Door is Open sensor
+GPIO.setup(pinconfig.DOOR3_CLOSED_SENSOR, GPIO.IN, GPIO.PUD_DOWN)  # Door is Closed sensor
+GPIO.setup(pinconfig.DOOR3_OPEN_SENSOR, GPIO.IN, GPIO.PUD_DOWN)    # Door is Open sensor
 
 try:
   while 1 >=0:
